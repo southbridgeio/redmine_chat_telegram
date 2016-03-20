dc51859c86260555bfee45b209e401fa40155837
Igor Olemskoi <igor@southbridge.ru>
2016-03-20 21:31:57 +0300

    Update redmine_chat_telegram.rb

--- lib/redmine_chat_telegram.rb
+++ lib/redmine_chat_telegram.rb
@@ -3,12 +3,12 @@ require 'telegram'
 
 class RedmineChatTelegram
   def first
-    cli_base = '/Users/artur/projects/tg/bin/telegram-cli -W -k /Users/artur/projects/tg/tg-server.pub -e '
+    cli_base = '/usr/bin/telegram-cli -W -k /etc/telegram-cli/server.pub -e '
 
 
     subject = "Название проекта #114117"
 
-    puts %x(#{cli_base} "create_group_chat \\"#{subject}\\" CentosadminBot" )
+    puts %x(#{cli_base} "create_group_chat \\"#{subject}\\" SouthbridgeBot" )
 
     cmd = "#{cli_base} \"export_chat_link #{subject.gsub(' ', '_').gsub('#', '@')}\""
     result = %x(#{cmd} )
