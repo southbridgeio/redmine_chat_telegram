62d39d8e9bceb5d4683cc04cb9ecd7281f1626df
Artur Trofimov <arturtr@gmail.com>
2016-04-11 11:17:04 +1000

    Publish report only if messages present

--- app/workers/telegram_group_daily_report_worker.rb
+++ app/workers/telegram_group_daily_report_worker.rb
@@ -16,24 +16,26 @@ class TelegramGroupDailyReportWorker
         where('sent_at >= ? and sent_at <= ?', time_from, time_to).
         where(is_system: false, bot_message: false)
 
-    date_string       = format_date(yesterday)
-    user_names        = telegram_messages.map(&:author_name).uniq
-    joined_user_names = user_names.join(', ')
-    journal_text      =
-        "_#{ I18n.t 'redmine_chat_telegram.journal.from_telegram' }:_ \n\n" +
-            I18n.t('redmine_chat_telegram.journal.daily_report',
-                   date:           date_string,
-                   users:          joined_user_names,
-                   messages_count: telegram_messages.size,
-                   users_count:    user_names.count)
+    if telegram_messages.present?
+      date_string       = format_date(yesterday)
+      user_names        = telegram_messages.map(&:author_name).uniq
+      joined_user_names = user_names.join(', ')
+      journal_text      =
+          "_#{ I18n.t 'redmine_chat_telegram.journal.from_telegram' }:_ \n\n" +
+              I18n.t('redmine_chat_telegram.journal.daily_report',
+                     date:           date_string,
+                     users:          joined_user_names,
+                     messages_count: telegram_messages.size,
+                     users_count:    user_names.count)
 
 
-    begin
-      issue.init_journal(User.anonymous, journal_text)
-      issue.save
-    rescue ActiveRecord::StaleObjectError
-      issue.reload
-      retry
+      begin
+        issue.init_journal(User.anonymous, journal_text)
+        issue.save
+      rescue ActiveRecord::StaleObjectError
+        issue.reload
+        retry
+      end
     end
   rescue ActiveRecord::RecordNotFound => e
     # ignore
