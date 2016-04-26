03b9361f695b4aa6bc9db40a75a21bce9485419d
Artur Trofimov <arturtr@gmail.com>
2016-04-26 10:22:21 +1000

    Little refactoring, dev version increment

--- app/workers/telegram_issue_notifications_worker.rb
+++ app/workers/telegram_issue_notifications_worker.rb
@@ -5,9 +5,10 @@ class TelegramIssueNotificationsWorker
   include ActionView::Helpers::TagHelper
   include ActionView::Helpers::UrlHelper
   include ERB::Util
-  delegate :link_to, :to => 'ActionController::Base.helpers'
+  delegate :link_to, to: 'ActionController::Base.helpers'
 
-  TELEGRAM_ISSUE_NOTIFICATIONS_LOG = Logger.new(Rails.root.join('log/chat_telegram', 'telegram-issue-notifications.log'))
+  TELEGRAM_ISSUE_NOTIFICATIONS_LOG =
+      Logger.new(Rails.root.join('log/chat_telegram', 'telegram-issue-notifications.log'))
 
   def perform(telegram_id, journal_id)
     ActionView::Base.send(:include, Rails.application.routes.url_helpers)
@@ -15,13 +16,11 @@ class TelegramIssueNotificationsWorker
 
     journal = Journal.find(journal_id)
 
-    if journal.details.any?
-      message = "#{ details_to_strings(journal.visible_details).join("\n") }"
-      message << "\n" << "<pre>#{ journal.notes }</pre>" unless journal.notes.blank?
-    else
-      message = "<pre>#{ journal.notes }</pre>"
-    end
-    message.prepend("<b>#{journal.user.name}</b>\n")
+    message = "<b>#{journal.user.name}</b>"
+
+    message << "\n#{ details_to_strings(journal.visible_details).join("\n") }" if journal.details.present?
+
+    message << "\n<pre>#{ journal.notes }</pre>" if journal.notes.present?
 
     TelegramMessageSenderWorker.perform_async(telegram_id, message)
   rescue ActiveRecord::RecordNotFound => e
