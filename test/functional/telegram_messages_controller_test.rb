d087eaa7b94e2dd07dbb564924ef59dcb32a676a
Stepan Lusnikov <endenwer@gmail.com>
2016-10-31 11:25:13 +0300

    Clean tests

--- test/functional/telegram_messages_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path('../../test_helper', __FILE__)
-
-class TelegramMessagesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
-end
