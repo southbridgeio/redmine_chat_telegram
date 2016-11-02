134365ca2e240e47818b95f4e28df78a4edc156b
Stepan Lusnikov <endenwer@gmail.com>
2016-11-02 21:09:19 +0300

    Fix migrations

--- db/migrate/012_add_token_to_telegram_account.rb
+++ db/migrate/012_add_token_to_telegram_account.rb
@@ -1,7 +1,6 @@
 class AddTokenToTelegramAccount < ActiveRecord::Migration
   def up
     add_column :redmine_chat_telegram_accounts, :token, :string
-    RedmineChatTelegram::Account.find_each { |telegram_account| telegram_account.update_attribute(:token, Redmine::Utils.random_hex(20)) }
   end
 
   def down
