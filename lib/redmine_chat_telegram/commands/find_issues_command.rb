a0830e7c4d397e243cc05626ef0c8d54a6bb2eed
Stepan Lusnikov <endenwer@gmail.com>
2016-11-07 18:45:39 +0300

    Add /deadline command

--- lib/redmine_chat_telegram/commands/find_issues_command.rb
+++ lib/redmine_chat_telegram/commands/find_issues_command.rb
@@ -2,27 +2,32 @@ module RedmineChatTelegram
   module Commands
     class FindIssuesCommand < BaseBotCommand
 
-      MESSAGES = {
-        'hot' => "*Назначенные вам задачи с активностью за последние сутки:*\n",
-        'me'  => "*Назначенные вам задачи:*\n"
-      }
-
       def execute
         if issues.count > 0
           bot.send_message(chat_id: command.chat.id, text: issues_list, parse_mode: 'Markdown')
         else
-          bot.send_message(chat_id: command.chat.id, text: "Не найдено задач.")
+          issues_not_found = I18n.t('redmine_chat_telegram.bot.issues_not_found')
+          bot.send_message(chat_id: command.chat.id, text: issues_not_found)
         end
       end
 
       private
 
       def command_name
-        @command_name ||= command.text.match(/^\/(\w+)/)[1]
+        case command.text
+        when /\/hot/
+          'hot'
+        when /\/me/
+          'me'
+        when /\/deadline|\/dl/
+          'deadline'
+        else
+          nil
+        end
       end
 
       def message
-        @message ||= MESSAGES[command_name]
+        @message ||= I18n.t("redmine_chat_telegram.bot.#{ command_name }")
       end
 
       def issues
@@ -30,18 +35,19 @@ module RedmineChatTelegram
       end
 
       def issue_filters
+        assigned_to_me = Issue.open.where(assigned_to: account.user)
         {
-          'me'  => Issue.open.where(assigned_to: account.user),
-          'hot' => Issue.open
-                  .where(assigned_to: account.user)
-                  .where('updated_on >= ?', 24.hours.ago),
+          'me' => assigned_to_me,
+          'hot' => assigned_to_me.where('updated_on >= ?', 24.hours.ago),
+          'deadline' => assigned_to_me.where('due_date < ?', Date.today)
         }
       end
 
       def issues_list
-        issues.inject(message) do |issues_list, issue|
+        message_title = "*#{ message }:*\n"
+        issues.inject(message_title) do |message_text, issue|
           url = Rails.application.routes.url_helpers.issue_url(issue, host: Setting.host_name)
-          issues_list << "#{ issue.subject }: #{ url }\n"
+          message_text << "#{ issue.subject }: #{ url }\n"
         end
       end
     end
