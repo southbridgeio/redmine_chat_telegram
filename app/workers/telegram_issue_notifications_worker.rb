33367ad58407891b94b72e81fde4e0cd9bc6634b
cross <cross@vgg.ru>
2017-01-02 20:03:07 +0300

    Add expectation before TelegramIssueNotifications.

--- app/workers/telegram_issue_notifications_worker.rb
+++ app/workers/telegram_issue_notifications_worker.rb
@@ -2,11 +2,13 @@ class TelegramIssueNotificationsWorker
   include Sidekiq::Worker
   include IssuesHelper
 
-  TELEGRAM_ISSUE_NOTIFICATIONS_LOG =
-    Logger.new(Rails.root.join('log/chat_telegram', 'telegram-issue-notifications.log'))
+  TELEGRAM_ISSUE_NOTIFICATIONS_LOG = Logger.new(Rails.root.join('log/chat_telegram', 'telegram-issue-notifications.log'))
 
   def perform(telegram_id, journal_id)
     RedmineChatTelegram.set_locale
+
+    TELEGRAM_ISSUE_NOTIFICATIONS_LOG.info "TELEGRAM_ID: #{telegram_id}, JOURNAL_ID: #{journal_id}"
+    sleep 1
 
     journal = Journal.find(journal_id)
 
@@ -16,8 +18,10 @@ class TelegramIssueNotificationsWorker
 
     message << "\n<pre>#{journal.notes}</pre>" if journal.notes.present?
 
+    TELEGRAM_ISSUE_NOTIFICATIONS_LOG.info "MESSAGE: #{message}"
+
     TelegramMessageSenderWorker.perform_async(telegram_id, message)
   rescue ActiveRecord::RecordNotFound => e
-    # ignore
+    TELEGRAM_ISSUE_NOTIFICATIONS_LOG.error "ERROR: #{e.message}"
   end
 end
