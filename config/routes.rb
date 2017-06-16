a5dac0976b5eb9396d6c0a7bdb70b525e140a9ce
constXife <constxife@yandex.ru>
2017-06-16 21:23:45 +0700

    Update web hook route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ post 'issues/:id/telegram_messages/publish' => 'telegram_messages#publish', as: 
 
 scope :redmine_chat_telegram do
   scope :api do
-    get 'web_hook' => 'telegram_api#web_hook', as: 'chat_telegram_api_webhook'
+    post 'web_hook' => 'telegram_api#web_hook', as: 'chat_telegram_api_webhook'
     post 'bot_init' => 'telegram_api#bot_init', as: 'chat_telegram_api_bot_init'
     delete 'bot_dinit' => 'telegram_api#bot_deinit', as: 'chat_telegram_api_bot_deinit'
 
