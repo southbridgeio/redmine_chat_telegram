b1d21489a7e478519e38ec2003517bd99d940f02
constXife <constxife@yandex.ru>
2017-06-18 13:06:47 +0700

    Store messages

--- app/controllers/telegram_api_controller.rb
+++ app/controllers/telegram_api_controller.rb
@@ -7,8 +7,31 @@ class TelegramApiController < ApplicationController
   layout false
 
   def web_hook
-    command = Telegram::Bot::Types::Update.new(params).message
-    RedmineChatTelegram::Bot.new(command).call if command.is_a?(Telegram::Bot::Types::Message)
+    message = Telegram::Bot::Types::Update.new(params).message
+    RedmineChatTelegram::Bot.new(message).call if message.is_a?(Telegram::Bot::Types::Message)
+
+    group = RedmineChatTelegram::TelegramGroup.find_by(telegram_id: message.chat.id.abs)
+
+    if group.present?
+      sent_at = Time.at message.date
+
+      from = message.from
+      from_id = from.id
+      from_first_name = from.first_name
+      from_last_name = from.last_name
+      from_username = from.username
+
+      message_text = message.text
+
+      TelegramMessage.where(telegram_id: message.message_id)
+          .first_or_create issue_id: group.issue.id,
+                           sent_at: sent_at,
+                           from_id: from_id,
+                           from_first_name: from_first_name,
+                           from_last_name: from_last_name,
+                           from_username: from_username,
+                           message: message_text
+    end
 
     head :ok, content_type: 'text/html'
   end
