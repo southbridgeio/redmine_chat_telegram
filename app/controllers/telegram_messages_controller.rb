1d3e39262e97deed4a3999c27e5518a8481a588b
constXife <constxife@yandex.ru>
2017-06-29 19:22:40 +0700

    Add reverse scope for messages

--- app/controllers/telegram_messages_controller.rb
+++ app/controllers/telegram_messages_controller.rb
@@ -14,7 +14,7 @@ class TelegramMessagesController < ApplicationController
 
   def publish
     @issue = Issue.visible.find(params[:id])
-    @telegram_messages = @issue.telegram_messages.where(id: params[:telegram_message_ids])
+    @telegram_messages = @issue.telegram_messages.where(id: params[:telegram_message_ids]).reverse_scope
     @issue.init_journal(User.current, @telegram_messages.as_text)
     @issue.save
     redirect_to @issue
