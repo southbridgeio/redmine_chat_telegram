41386fde9afdbf6aa0d0d6d70ce660fd354414b3
constXife <constxife@yandex.ru>
2017-07-10 21:42:01 +0700

    Remove telegram requests from log

--- app/workers/telegram_handler_worker.rb
+++ app/workers/telegram_handler_worker.rb
@@ -6,7 +6,6 @@ class TelegramHandlerWorker
     message = Telegram::Bot::Types::Update.new(params).message
 
     if message.present?
-      logger.debug params.to_json
       RedmineChatTelegram.handle_message(message)
     else
       logger.fatal "Can't find message: #{params.to_json}"
