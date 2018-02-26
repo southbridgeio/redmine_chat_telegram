d379ba8e6b41a286809e6395a158e5ace2569494
Vladislav Yashin <v.yashin.work@gmail.com>
2018-02-26 15:49:35 +0300

    Fix tests

--- test/unit/redmine_chat_telegram/common_integration_bot_test.rb
+++ test/unit/redmine_chat_telegram/common_integration_bot_test.rb
@@ -3,69 +3,6 @@ require File.expand_path('../../../test_helper', __FILE__)
 class RedmineChatTelegram::CommonIntegrationBotTest < ActiveSupport::TestCase
   fixtures :users, :email_addresses, :roles
 
-  context '/start' do
-    setup do
-      @telegram_message = Telegram::Bot::Types::Message.new(
-        from: { id:         123,
-                username:   'dhh',
-                first_name: 'David',
-                last_name:  'Haselman' },
-        chat: { id: 123,
-                type: 'private' },
-        text: '/start'
-      )
-
-      @bot_service = RedmineChatTelegram::Bot.new(@telegram_message)
-    end
-
-    context 'without user' do
-      setup do
-        RedmineChatTelegram::Bot.any_instance
-            .expects(:send_message)
-            .with(I18n.t('telegram_common.bot.start.instruction_html'))
-      end
-
-      should 'create telegram account' do
-        assert_difference('TelegramCommon::Account.count') do
-          @bot_service.call
-        end
-
-        telegram_account = TelegramCommon::Account.last
-        assert_equal 123, telegram_account.telegram_id
-        assert_equal 'dhh', telegram_account.username
-        assert_equal 'David', telegram_account.first_name
-        assert_equal 'Haselman', telegram_account.last_name
-        assert telegram_account.active
-      end
-
-      should 'update telegram account' do
-        telegram_account = TelegramCommon::Account.create(telegram_id: 123, username: 'test', first_name: 'f', last_name: 'l')
-
-        assert_no_difference('TelegramCommon::Account.count') do
-          @bot_service.call
-        end
-
-        telegram_account.reload
-
-        assert_equal 'dhh', telegram_account.username
-        assert_equal 'David', telegram_account.first_name
-        assert_equal 'Haselman', telegram_account.last_name
-      end
-
-      should 'activate telegram account' do
-        actual = TelegramCommon::Account.create(telegram_id: 123, active: false)
-
-        assert_no_difference('TelegramCommon::Account.count') do
-          @bot_service.call
-        end
-
-        actual.reload
-
-        assert actual.active
-      end
-    end
-  end
-
   context 'wrong command context' do
     context 'private' do
       setup do
@@ -131,8 +68,7 @@ class RedmineChatTelegram::CommonIntegrationBotTest < ActiveSupport::TestCase
       should 'send help for private chat' do
         RedmineChatTelegram::Bot.any_instance.stubs(:private_ext_commands).returns([])
         text = <<~TEXT
-          /start - #{I18n.t('redmine_chat_telegram.bot.private.help.start')}
-          /connect - #{I18n.t('redmine_chat_telegram.bot.private.help.connect')}
+          Redmine Chat Telegram:
           /help - #{I18n.t('redmine_chat_telegram.bot.private.help.help')}
           /new - #{I18n.t('redmine_chat_telegram.bot.private.help.new')}
           /hot - #{I18n.t('redmine_chat_telegram.bot.private.help.hot')}
@@ -169,6 +105,7 @@ class RedmineChatTelegram::CommonIntegrationBotTest < ActiveSupport::TestCase
 
       should 'send help for private chat' do
         text = <<~TEXT
+          Redmine Chat Telegram:
           /help - #{I18n.t('redmine_chat_telegram.bot.group.help.help')}
           /task - #{I18n.t('redmine_chat_telegram.bot.group.help.task')}
           /link - #{I18n.t('redmine_chat_telegram.bot.group.help.link')}
